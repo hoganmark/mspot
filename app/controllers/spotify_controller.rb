2902d6cb18846481ca31204da2265e25b47418f4
Mark Hogan <mrkthgn@gmail.com>
2021-10-25 13:57:55 -0400

    index

--- app/controllers/spotify_controller.rb
+++ app/controllers/spotify_controller.rb
@@ -1,5 +1,13 @@
 class SpotifyController < ApplicationController
   def connect
+    if user
+      redirect_to action: :index
+      return
+    end
+  end
+
+  def logout
+    session[:user_id] = nil
   end
 
   def callback
@@ -7,5 +15,24 @@ class SpotifyController < ApplicationController
     @spotify_user = RSpotify::User.new auth_hash
     user = User.find_or_create_by email: @spotify_user.email
     user.update! auth_hash: auth_hash.to_h, userid: @spotify_user.id
+    session[:user_id] = user.id
+
+    redirect_to action: :index
+  end
+
+  def index
+    if user.nil?
+      redirect_to action: :connect
+      return
+    end
+
+    @spotify_user = user.spotify_user
+  end
+
+  def user
+    return @user if @user
+
+    user_id = session[:user_id]
+    @user = User.find_by(id: user_id) if user_id
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get '/connect', to: 'spotify#connect'
+  get '/logout', to: 'spotify#logout'
   get '/auth/spotify/callback', to: 'spotify#callback'
+  get '/', to: 'spotify#index'
 end
