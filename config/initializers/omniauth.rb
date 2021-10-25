b265924729a0891cb5302dd8eb09374ea960e2a7
Mark Hogan <mrkthgn@gmail.com>
2021-10-25 15:59:55 -0400

    last played

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 require 'rspotify/oauth'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, "f7a0c2e64f54415783aece9fb5bbe7c0", "7de002acff4d4b679738f2b3381776d2", scope: 'user-read-email playlist-modify-public user-library-read user-library-modify user-top-read'
+  provider :spotify, "f7a0c2e64f54415783aece9fb5bbe7c0", "7de002acff4d4b679738f2b3381776d2", scope: 'user-read-email playlist-modify-public user-library-read user-library-modify user-top-read user-read-recently-played
+'
 end
