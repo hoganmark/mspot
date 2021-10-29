a4de0b7c43a6feb7a7c7201c497c32aaa2d87a42
Mark Hogan <mrkthgn@gmail.com>
2021-10-29 07:59:36 -0400

    scopes!

--- app/models/track.rb
+++ app/models/track.rb
@@ -7,6 +7,28 @@ class Track < ApplicationRecord
 
   enum mode: { minor: 0, major: 1 }
 
+  scope :in_key_of, ->(key) { where(key: key) }
+  scope :time, ->(time) { where(time: time) }
+
+  scope :slow, -> { where('tempo <= 110')}
+  scope :medium_pace, -> { where('tempo > 110 and tempo <= 135')}
+  scope :fast, -> { where('tempo > 135')}
+
+  scope :short, -> { where('duration < 180') }
+  scope :medium_length, -> { where('duration >= 180 and duration < 240') }
+  scope :long, -> { where('duration >= 240') }
+
+  scope :acoustic, -> { where(acoustic: true) }
+  scope :non_acoustic, -> { where(acoustic: false) }
+
+  scope :old, -> { joins(:album).where("albums.year < 1960") }
+  scope :sixties, -> { joins(:album).where("albums.year >= 1960 and albums.year < 1970") }
+  scope :seventies, -> { joins(:album).where("albums.year >= 1970 and albums.year < 1980") }
+  scope :eighties, -> { joins(:album).where("albums.year >= 1980 and albums.year < 1990") }
+  scope :nineties, -> { joins(:album).where("albums.year >= 1990 and albums.year < 2000") }
+  scope :aughts, -> { joins(:album).where("albums.year >= 2000 and albums.year < 2010") }
+  scope :recent, -> { joins(:album).where("albums.year >= 2010") }
+
   def set_audio_features
     return unless audio_features.present?
 
