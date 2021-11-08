0932c1daaebae0f11eebecd6ec52227ddf60ea23
Mark Hogan <mrkthgn@gmail.com>
2021-11-08 13:35:22 -0500

    artists can have > 50 albums

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,49 +26,55 @@ class User < ApplicationRecord
 
     user_artists.find_or_create_by!(artist: artist)
 
-    spotify_artist.albums(limit: 50, album_type: :album).select{|al| al.artists.map(&:name) == [artist.name]}.each do |spotify_album|
-      next if albums.exists? uri: spotify_album.uri
-      upc = spotify_album.external_ids&.dig('upc')&.to_i&.to_s # remove leading zeros
-      next if upc && albums.exists?(upc: upc)
-      next if albums.exists? artist_id: artist.id, name: spotify_album.name
-      next unless spotify_album.available_markets.include? country
-      next if spotify_album.album_type == 'compilation' # still getting some despite query for some reason
-      next if RSpotify::Album.find(spotify_album.id).inspect.downcase['compilation']
-      tracks = spotify_album.tracks(limit: 50).select{|track| track.artists.map(&:name).include?(artist.name)}
-      next if tracks.size < 6 # LPs only please
-      next if upc.in? Album.ignored_upcs
+    offset = 0
+    while true
+      spotify_albums = spotify_artist.albums(limit: 50, offset: offset, album_type: :album).select{|al| al.artists.map(&:name) == [artist.name]}
+      break if spotify_albums.count == 0
+      spotify_albums.each do |spotify_album|
+        next if albums.exists? uri: spotify_album.uri
+        upc = spotify_album.external_ids&.dig('upc')&.to_i&.to_s # remove leading zeros
+        next if upc && albums.exists?(upc: upc)
+        next if albums.exists? artist_id: artist.id, name: spotify_album.name
+        next unless spotify_album.available_markets.include? country
+        next if spotify_album.album_type == 'compilation' # still getting some despite query for some reason
+        next if RSpotify::Album.find(spotify_album.id).inspect.downcase['compilation']
+        tracks = spotify_album.tracks(limit: 50).select{|track| track.artists.map(&:name).include?(artist.name)}
+        next if tracks.size < 6 # LPs only please
+        next if upc.in? Album.ignored_upcs
 
-      album = artist.albums.find_by uri: spotify_album.uri
-      unless album
-        album = artist.albums.create!(uri: spotify_album.uri) do |al|
-          al.name = spotify_album.name
-          al.year = spotify_album.release_date.split('-').first.to_i
-          al.image = spotify_album.images.first['url'] if spotify_album.images&.first
-          al.available_markets = spotify_album.available_markets
-          al.upc = upc
+        album = artist.albums.find_by uri: spotify_album.uri
+        unless album
+          album = artist.albums.create!(uri: spotify_album.uri) do |al|
+            al.name = spotify_album.name
+            al.year = spotify_album.release_date.split('-').first.to_i
+            al.image = spotify_album.images.first['url'] if spotify_album.images&.first
+            al.available_markets = spotify_album.available_markets
+            al.upc = upc
+          end
+
+          tracks.each do |spotify_track|
+            audio_features = spotify_track.audio_features rescue nil
+            album.tracks.create! \
+              uri: spotify_track.uri,
+              name: spotify_track.name,
+              number: spotify_track.track_number,
+              popularity: spotify_track.popularity,
+              audio_features: audio_features,
+              url: spotify_track.external_urls&.values&.first
+          end
+
+          corrected_year = Album.corrected_years[spotify_album.id]
+          album.update! year: corrected_year if corrected_year
+
+          if (album.tracks.map{|t| t.audio_features&.dig('liveness')}.compact.sum / album.tracks.count) > 0.6
+            album.update! live: true
+          end
         end
+        user_albums.create!(album: album)
 
-        tracks.each do |spotify_track|
-          audio_features = spotify_track.audio_features rescue nil
-          album.tracks.create! \
-            uri: spotify_track.uri,
-            name: spotify_track.name,
-            number: spotify_track.track_number,
-            popularity: spotify_track.popularity,
-            audio_features: audio_features,
-            url: spotify_track.external_urls&.values&.first
-        end
-
-        corrected_year = Album.corrected_years[spotify_album.id]
-        album.update! year: corrected_year if corrected_year
-
-        if (album.tracks.map{|t| t.audio_features&.dig('liveness')}.sum / album.tracks.count) > 0.6
-          album.update! live: true
-        end
+        sleep sleep_after_album_s
       end
-      user_albums.create!(album: album)
-
-      sleep sleep_after_album_s
+      offset += 50
     end
 
     hide_special_editions!(artist)
