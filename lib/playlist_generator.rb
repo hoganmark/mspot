1c62a69b8a5441045321de927622978a3f3668be
Mark Hogan <mrkthgn@gmail.com>
2021-11-13 23:57:23 -0500

    more playlists

--- lib/playlist_generator.rb
+++ lib/playlist_generator.rb
@@ -6,12 +6,17 @@ class PlaylistGenerator
   end
 
   def generate!
-    %w[slow steady fast short long acoustic live sixties eighties].each {|scope| generate_with_scope(scope) }
+    %w[slow fast short long acoustic live sixties eighties].each {|scope| generate_with_scopes([scope]) }
+    generate_with_scopes(%w[fast short studio major], slug: 'short n sweet')
+    generate_with_scopes(%w[slow long acoustic], slug: :chill)
+    generate_with_scopes([], slug: :random)
     true
   end
 
-  def generate_with_scope(scope)
-    playlist = user.playlists.find_or_create_by slug: scope
-    playlist.spotify_playlist.replace_tracks! user.tracks.send(scope).order(popularity: :desc).first(100).sample(20)
+  def generate_with_scopes(scopes, slug: scopes.first)
+    playlist = user.playlists.find_or_create_by slug: slug
+    tracks = user.tracks.order(popularity: :desc)
+    scopes.each {|scope| tracks = tracks.send(scope)}
+    playlist.spotify_playlist.replace_tracks! tracks.first(100).sample(20)
   end
 end
